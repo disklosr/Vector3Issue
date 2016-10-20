40c6768955cb5eff1ba252c6017063b2c1ac4fff
Anas Mazioudi <disklosr@outlook.com>
2016-10-20 22:12:21 +0200

    Initial commit

--- Vector3Issue/MainPage.xaml.cs
+++ Vector3Issue/MainPage.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using System.Numerics;
+using Windows.UI;
+using Windows.UI.Composition;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Hosting;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
---
+++ Vector3Issue/ShadowControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Numerics;
+using Windows.UI.Composition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Hosting;
+
+namespace Vector3Issue
+{
+    public sealed partial class ShadowControl : ContentControl
+    {
+        public ShadowControl()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            var source = (Grid)GetTemplateChild("CardRoot");
+            var target = (Canvas)GetTemplateChild("ShadowHost");
+
+            InitializeDropShadow(source, target);
+
+            base.OnApplyTemplate();
+        }
+
+        private void InitializeDropShadow(UIElement shadowShape, UIElement shadowHost)
+        {
+            Visual hostVisual = ElementCompositionPreview.GetElementVisual(shadowShape);
+            Compositor compositor = hostVisual.Compositor;
+
+            var dropShadow = compositor.CreateDropShadow();
+
+            //Commenting this line will make the xaml designer of MainPage.xaml happy
+            dropShadow.Offset = new Vector3(0f, 2f, 2f);
+        }
+    }
+}
